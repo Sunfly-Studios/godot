2626539d7b06061f78890f3a5ace30272de7d697
kobewi <kobewi4e@gmail.com>
2025-02-19 14:27:27 +0100

    Replace UID and Surface upgrade tools with universal one

--- editor/project_upgrade_tool.h
+++ editor/project_upgrade_tool.h
@@ -1,5 +1,5 @@
 /**************************************************************************/
-/*  surface_upgrade_tool.h                                                */
+/*  project_upgrade_tool.h                                                */
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
@@ -31,52 +31,35 @@
 #ifndef SURFACE_UPGRADE_TOOL_H
 #define SURFACE_UPGRADE_TOOL_H
 
-#include "scene/gui/dialogs.h"
+#include "core/object/class_db.h"
 
+class ConfirmationDialog;
 class EditorFileSystemDirectory;
 
-class SurfaceUpgradeTool : public Object {
-	GDCLASS(SurfaceUpgradeTool, Object);
+class ProjectUpgradeTool : public Object {
+	GDCLASS(ProjectUpgradeTool, Object);
 
-	static SurfaceUpgradeTool *singleton;
+	ConfirmationDialog *upgrade_dialog = nullptr;
 
-	Mutex mutex;
+	void _add_files(EditorFileSystemDirectory *p_dir, Vector<String> &r_reimport_paths, Vector<String> &r_resave_scenes, Vector<String> &r_resave_resources);
 
-	bool show_requested = false;
-	bool updating = false;
+	const String META_REIMPORT_PATHS = "reimport_paths";
+	const String META_RESAVE_SCENES = "resave_scenes";
+	const String META_RESAVE_RESOURCES = "resave_resources";
 
-	static void _try_show_popup();
-	void _show_popup();
-
-	void _add_files(EditorFileSystemDirectory *p_dir, Vector<String> &r_reimport_paths, Vector<String> &r_resave_paths);
+public:
+	const String META_PROJECT_UPGRADE_TOOL = "project_upgrade_tool";
+	const String META_RUN_ON_RESTART = "run_on_restart";
+	const StringName UPGRADE_FINISHED = "upgrade_finished";
 
 protected:
 	static void _bind_methods();
 
 public:
-	static SurfaceUpgradeTool *get_singleton() { return singleton; }
-
-	bool is_show_requested() const { return show_requested; }
-	void show_popup() { _show_popup(); }
-
+	void popup_dialog();
 	void prepare_upgrade();
 	void begin_upgrade();
 	void finish_upgrade();
-
-	SurfaceUpgradeTool();
-	~SurfaceUpgradeTool();
-};
-
-class SurfaceUpgradeDialog : public ConfirmationDialog {
-	GDCLASS(SurfaceUpgradeDialog, ConfirmationDialog);
-
-protected:
-	void _notification(int p_what);
-
-public:
-	void popup_on_demand();
-
-	SurfaceUpgradeDialog();
 };
 
 #endif // SURFACE_UPGRADE_TOOL_H
