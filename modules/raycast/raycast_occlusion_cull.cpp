e6e0eb295a148273a7519da0aa6d8d0cfa67c85e
MBCX <mbcx@debianbuildsystem.buildsystem.org>
2025-05-30 17:13:20 -0400

    Fix silly mistake with MIPS variable change

--- modules/raycast/raycast_occlusion_cull.cpp
+++ modules/raycast/raycast_occlusion_cull.cpp
@@ -175,7 +175,7 @@ void RaycastOcclusionCull::RaycastHZBuffer::sort_rays(const Vector3 &p_camera_di
 					int tile_index = i * tile_grid_size.x + j;
 
 					Vector3 ray_dir(camera_rays[tile_index].ray.dir_x[k], camera_rays[tile_index].ray.dir_y[k], camera_rays[tile_index].ray.dir_z[k]);
-					mips[0][y * buffer_size.x + x] = camera_rays[tile_index].ray.tfar[k] * p_camera_dir.dot(ray_dir); // Store z-depth in view space.
+					mips_levels[0][y * buffer_size.x + x] = camera_rays[tile_index].ray.tfar[k] * p_camera_dir.dot(ray_dir); // Store z-depth in view space.
 				}
 			}
 		}
