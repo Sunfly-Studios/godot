50e74d0187db85d8d37c0bee099b2e3067c408d5
Anish Kumar <mishragames@gmail.com>
2026-01-22 16:44:21 +0530

    Enable native file picker support on Android 9 and older

--- platform/android/java/lib/src/org/godotengine/godot/Godot.kt
+++ platform/android/java/lib/src/org/godotengine/godot/Godot.kt
@@ -802,10 +802,8 @@ class Godot(private val context: Context) {
 		for (plugin in pluginRegistry.allPlugins) {
 			plugin.onMainActivityResult(requestCode, resultCode, data)
 		}
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-			runOnRenderThread {
-				FilePicker.handleActivityResult(context, requestCode, resultCode, data)
-			}
+		runOnRenderThread {
+			FilePicker.handleActivityResult(context, requestCode, resultCode, data)
 		}
 	}
 
@@ -1036,9 +1034,7 @@ class Godot(private val context: Context) {
 
 	@Keep
 	private fun showFilePicker(currentDirectory: String, filename: String, fileMode: Int, filters: Array<String>) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-			FilePicker.showFilePicker(context, getActivity(), currentDirectory, filename, fileMode, filters)
-		}
+		FilePicker.showFilePicker(context, getActivity(), currentDirectory, filename, fileMode, filters)
 	}
 
 	/**
