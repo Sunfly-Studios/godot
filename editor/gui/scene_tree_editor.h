e151813ab1d719d58f6a1bb78f3d59171665b6f6
MBCX <github.small792@passinbox.com>
2026-01-28 20:41:53 -0400

    Optimize tree size computation and the scene tree dock filter (PR #110759)

--- editor/gui/scene_tree_editor.h
+++ editor/gui/scene_tree_editor.h
@@ -145,6 +145,7 @@ class SceneTreeEditor : public Control {
 
 	void _test_update_tree();
 	bool _update_filter(TreeItem *p_parent = nullptr, bool p_scroll_to_selected = false);
+	bool _update_filter_helper(TreeItem *p_parent, bool p_scroll_to_selected, TreeItem *&r_last_selected);
 	bool _item_matches_all_terms(TreeItem *p_item, const PackedStringArray &p_terms);
 	void _tree_changed();
 	void _tree_process_mode_changed();
