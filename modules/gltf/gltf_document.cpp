V-Sekai-fire/multiplayer-fabric-godot

GLTF: Don't serialize empty material extensions

modules/gltf/gltf_document.cpp
@@ -4661,7 +4661,9 @@ Error GLTFDocument::_serialize_materials(Ref<GLTFState> p_state) {
 			extensions["KHR_materials_emissive_strength"] = mat_emissive_strength;
 			p_state->add_used_extension("KHR_materials_emissive_strength");
 		}
-		mat_dict["extensions"] = extensions;
+		if (!extensions.is_empty()) {
+			mat_dict["extensions"] = extensions;
+		}
 
 		_attach_meta_to_extras(material, mat_dict);
 		materials.push_back(mat_dict);

Sunfly-Studios/godot

modules/gltf/gltf_document.cpp
@@ -4664,7 +4664,9 @@ Error GLTFDocument::_serialize_materials(Ref<GLTFState> p_state) {
 			extensions["KHR_materials_emissive_strength"] = mat_emissive_strength;
 			p_state->add_used_extension("KHR_materials_emissive_strength");
 		}
-		d["extensions"] = extensions;
+		if (!extensions.is_empty()) {
+			d["extensions"] = extensions;
+		}
 
 		_attach_meta_to_extras(material, d);
 		materials.push_back(d);