c85dc6f2f959d77c6c2d3fc8b09dab93662e328d
A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
2026-03-04 18:53:01 +0100

    [Linux] Fix `addr2line` call

--- platform/linuxbsd/crash_handler_linuxbsd.cpp
+++ platform/linuxbsd/crash_handler_linuxbsd.cpp
@@ -98,19 +98,21 @@ static void handle_crash(int sig) {
 #endif //__GLIBC__
 	if (strings) {
 		int ret;
-		String output;
 
 		List<String> args;
 		args.push_back("--version");
 		String exe_name;
 
 		if (exe_name.is_empty()) {
+			String output;
+			// Faster implementation from gimli-rs/addr2line.
 			Error err = OS::get_singleton()->execute(OS::get_singleton()->get_environment("HOME").path_join(String("/.cargo/bin/addr2line")), args, &output, &ret);
 			if (err == OK && ret == 0) {
 				exe_name = OS::get_singleton()->get_environment("HOME").path_join(String("/.cargo/bin/addr2line"));
 			}
 		}
 		if (exe_name.is_empty()) {
+			String output;
 			Error err = OS::get_singleton()->execute(String("llvm-addr2line"), args, &output, &ret);
 			if (err == OK && ret == 0) {
 				exe_name = String("llvm-addr2line");
@@ -130,6 +132,7 @@ static void handle_crash(int sig) {
 		args.push_back(_execpath);
 
 		// Try to get the file/line number using addr2line
+		String output;
 		Error err = OS::get_singleton()->execute(exe_name, args, &output, &ret);
 		Vector<String> addr2line_results;
 		if (err == OK) {
