49fc172c3adb4d128d191334b7127f5ae51a03e0
MBCX <github.small792@passinbox.com>
2026-05-26 21:25:46 -0400

    [Android]: Missed variable declaration when building without GLES3 support.

--- platform/android/os_android.h
+++ platform/android/os_android.h
@@ -48,7 +48,7 @@ private:
 
 	bool use_apk_expansion;
 
-#if defined(GLES3_ENABLED)
+#if defined(GLES3_ENABLED) || defined(GLES2_ENABLED) || defined(GLES1_ENABLED)
 	const char *gl_extensions;
 #endif
 
