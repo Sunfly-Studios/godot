636ef5e89869c7f4265a81ec59f7170c50270065
MBCX <github.small792@passinbox.com>
2026-06-27 16:29:57 -0400

    [Core]: Fix missing newline to the end of the PEM String

--- core/config/project_settings.cpp
+++ core/config/project_settings.cpp
@@ -594,7 +594,7 @@ Error ProjectSettings::_setup(const String &p_path, const String &p_main_pack, b
 			w_bytes[i] = unaligned_read<uint8_t>(&pck_sign_pub_key[i]);
 		}
 
-		String str_key = "-----BEGIN PUBLIC KEY-----\n" + CryptoCore::b64_encode_str(aligned_key_buf.ptr(), pck_sign_pub_key_len) + "\n-----END PUBLIC KEY-----";
+		String str_key = "-----BEGIN PUBLIC KEY-----\n" + CryptoCore::b64_encode_str(aligned_key_buf.ptr(), pck_sign_pub_key_len) + "\n-----END PUBLIC KEY-----\n";
 		ERR_FAIL_COND_V_MSG(key->load_from_string(str_key, true) != OK, ERR_CANT_OPEN, vformat("Cannot open resource pack '%s'.", p_main_pack));
 	} else {
 		ERR_FAIL_V_MSG(ERR_CANT_OPEN, vformat("Cannot open resource pack '%s'.", p_main_pack));
