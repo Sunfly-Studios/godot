9885b74dd37b717b767980aab5083e41cca7ab92
MBCX <github.small792@passinbox.com>
2026-07-19 19:43:19 -0400

    [thirdparty/VMA]: Don't make integrated AMD GPUs crash (PR #120603)

--- thirdparty/vulkan/vk_mem_alloc.cpp
+++ thirdparty/vulkan/vk_mem_alloc.cpp
@@ -4,4 +4,9 @@
 #define _DEBUG
 #endif
 #endif
+
+// Makes integrated AMD GPUs not crash when Godot exceeds
+// more than 4096 allocations
+#define VMA_DEBUG_DONT_EXCEED_MAX_MEMORY_ALLOCATION_COUNT 0
+
 #include "vk_mem_alloc.h"
